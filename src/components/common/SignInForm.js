fc1e485f0f03f9530439450bdc572ca93b5a431e
Beniah Opong Diallo <136281757+Beniah1@users.noreply.github.com>
2024-03-07 18:05:49 +0000

    Update SigninForm and SignUpForm components to handle form submissions.

--- src/components/common/SignInForm.js
+++ src/components/common/SignInForm.js
@@ -1,11 +1,22 @@
-// LoginForm.js
 import React from "react";
 
 function SigninForm() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (!email || !password) {
+      alert("Email and password are required");
+    } else {
+      // Handle form submission
+    }
+  };
+
   return (
     <div>
       <h2 className="signin">Login</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="email" className="email">
           Email:
         </label>
--- src/components/common/SignUpForm.js
+++ src/components/common/SignUpForm.js
@@ -1,13 +1,26 @@
-// SignUpForm.js
 import React from "react";
 import "./SignUpForm.css"; // Import the CSS file
 import SigninForm from "./SignInForm";
 import { Link } from "react-router-dom";
+
 function SignUpForm() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (!email || !password) {
+      alert('Email and password are required');
+    } else {
+      alert('Thank you for signing up!');
+      // Handle form submission
+    }
+  };
+
   return (
     <div>
       <h2 className="signin">Sign Up</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="email" className="email">
           Email:
         </label>
@@ -21,12 +34,11 @@ function SignUpForm() {
           Sign Up
         </button>
         <br />
-
         <Link to="/signin">
-  <button type="submit" className="btn1">
-    Sign In
-  </button>
-</Link>
+          <button type="submit" className="btn1">
+            Sign In
+          </button>
+        </Link>
       </form>
     </div>
   );
