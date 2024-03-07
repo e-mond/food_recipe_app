6bcb7330ff62d75a535f6a240d7cab9b8da2a78d
Beniah Opong Diallo <136281757+Beniah1@users.noreply.github.com>
2024-03-07 17:05:40 +0000

    Added CSS file for styling the sign-up form and updated the form with labels, input fields, and submit buttons.

--- src/components/common/SignInForm.js
+++ src/components/common/SignInForm.js
@@ -5,6 +5,15 @@ function SigninForm() {
   return (
     <div>
       <h2>Login</h2>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <br />
+        <button type="submit">Login</button>
+      </form>
       {/* Add login form fields */}
     </div>
   );
--- src/components/common/SignUpForm.js
+++ src/components/common/SignUpForm.js
@@ -1,11 +1,30 @@
 // SignUpForm.js
 import React from "react";
-
+import "./SignUpForm.css"; // Import the CSS file
+import SigninForm from "./SignInForm";
+import { Link } from "react-router-dom";
 function SignUpForm() {
   return (
     <div>
-      <h2>Sign Up</h2>
-      {/* Add sign-up form fields */}
+      <h2 className="signin">Sign Up</h2>
+      <form>
+        <label htmlFor="email" className="email">
+          Email:
+        </label>
+        <input type="email" id="email" name="email" />
+        <br />
+        <label htmlFor="password" className="password">
+          Password:
+        </label>
+        <input type="password" id="password" name="password" />
+        <br />
+        <Link to="/SigninForm" className="btn1">
+          Sign In{" "}
+        </Link>
+        <button type="submit" className="btn1">
+          Sign Up
+        </button>
+      </form>
     </div>
   );
 }
