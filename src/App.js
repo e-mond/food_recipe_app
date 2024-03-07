bbee940f2c9a54885b4a09c13967300de3b7efaf
Beniah Opong Diallo <136281757+Beniah1@users.noreply.github.com>
2024-03-07 21:59:33 +0000

    new

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; // Import the useState and useEffect hooks
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { fetchRecipes } from './utils/api';
 import Header from './components/common/Header';
