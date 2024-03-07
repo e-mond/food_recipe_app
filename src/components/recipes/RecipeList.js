89070c325eb906a2e824d2cbcd0a174186ab25ff
Mond <126099833+e-mond@users.noreply.github.com>
2024-03-07 00:02:46 +0000

    Update

--- src/components/recipes/RecipeList.js
+++ src/components/recipes/RecipeList.js
@@ -1,37 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import RecipeCard from './RecipeCard';
-import { fetchRecipes } from '../../utils/api';
+// import { fetchRecipes } from '../../utils/api';
+import axios from 'axios';
+
 
 const RecipeList = ({ recipes, onSearch }) => {
-  const [fetchedRecipes, setFetchedRecipes] = useState([]);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (!recipes) {
-      // Fetch recipes only if recipes prop is null or undefined
-      fetchRecipes('jollof')
-        .then(data => {
-          console.log('API response:', data);
-          setFetchedRecipes(data || []); // Update state with fetched recipes or an empty array
-        })
-        .catch(error => {
-          console.error('Error fetching recipes:', error);
-        });
-    } else {
-      // Use the provided recipes prop if available
-      setFetchedRecipes(recipes);
-    }
-  }, [recipes]); // Re-run effect whenever recipes prop changes
+    const fetchData = async () => {
+        const apiId = '060315d5';
+        const apiKey = '779684a0718f8fbc41bb123d3fa1b1da';
+  
+        const url = `https://api.edamam.com/api/recipes/v2?type=public&q=jollof&app_id=${apiId}&app_key=${apiKey}`;
+  
+        try {
+          const response = await axios.get(url);
+          setData(response.data.hits);
+        console.log(response);
+          setLoading(false);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+          setLoading(false);
+        }
+      };
 
-  if (!fetchedRecipes || fetchedRecipes.length === 0) {
+    fetchData();
+  }, []); // Empty dependency array means it will only run once when the component mounts
+
+  if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
     <div className="recipe-list">
       <h2>Recipes</h2>
-      {fetchedRecipes.map(recipe => (
-        <Link to={`/recipe/${recipe.id}`} key={recipe.id}>
+      {data.map((recipe, index) => (
+        <Link to={`/recipe/${index}`} key={index}>
           <RecipeCard recipe={recipe} />
         </Link>
       ))}
